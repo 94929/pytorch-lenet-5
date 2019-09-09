6d42b3f1bee7b24f6a2f4dd3bdae612b1dd24269
Jinsung Ha <jinsung.ha929@gmail.com>
2019-09-09 20:04:37 +0900

    Define model but activation functions are missing

--- main.py
+++ main.py
@@ -41,3 +41,37 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           num_workers=2,
                                           drop_last=True)
                                           
+# model
+class LeNet5(nn.Module):
+    def __init__(self):
+        super(LeNet5, self).__init__()
+
+        C1 = nn.Conv2d(1, 6, kernel_size=5)
+        S2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        C3 = nn.Conv2d(6, 16, kernel_size=5)
+        S4 = nn.MaxPool2d(kernel_size=2, stride=2)
+        C5 = nn.Conv2d(16, 120, kernel_size=5)
+        F6 = nn.Linear(120, 84)
+        OUTPUT = nn.Linear(84, 10)
+
+        self.model = nn.Sequential(
+            C1,
+            S2,
+            C3,
+            S4,
+            C5,
+            F6,
+            OUTPUT
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+# model
+model = LeNet5().to(device)
+
+# loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+
